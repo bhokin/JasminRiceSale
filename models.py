76bcb8b144259ce13d3738c40c8c625631f9807e
Bhokin <bhokin.w@ku.th>
2022-04-26 19:07:35 +0700

    minor change cause of error

--- models.py
+++ models.py
@@ -1,12 +1,12 @@
-from sqlalchemy import Column, Text, Integer, ForeignKey, relationship
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Text, Integer, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship
 
 
 Base = declarative_base()
 
 
 class Customers(Base):
-    __tablename__ = "customer_account"
+    __tablename__ = "customers"
 
     id = Column(Integer, primary_key=True)
     first_name = Column(Text, nullable=False)
@@ -14,7 +14,7 @@ class Customers(Base):
     gender = Column(Text, nullable=False)
     age = Column(Integer, nullable=False)
 
-    jasmine_rice_sales = relationship("JasmineRiceSale", back_populates="costumer", cascade="all, delete-orphan")
+    jasmine_rice_sales = relationship("JasmineRiceSale", back_populates="customer", cascade="all, delete-orphan")
 
     def __repr__(self):
         return f"LottoTransaction (id={self.id}, first_name={self.first_name}, " \
@@ -28,7 +28,7 @@ class JasmineRiceSale(Base):
     weight_kg = Column(Integer, nullable=False)
     cost = Column(Integer, nullable=False)
     time = Column(Text, nullable=False)
-    customer_id = Column(Integer, ForeignKey("customer_account.id"), nullable=False)
+    customer_id = Column(Integer, ForeignKey("customers.id"), nullable=False)
 
     customer = relationship("Customers", back_populates="jasmine_rice_sales")
 

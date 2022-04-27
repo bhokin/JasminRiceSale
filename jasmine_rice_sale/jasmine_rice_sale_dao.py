046a365371428ec2787c5535e595604229a058d6
Bhokin <bhokin.w@ku.th>
2022-04-28 02:12:07 +0700

    Dao update

--- jasmine_rice_sale/jasmine_rice_sale_dao.py
+++ jasmine_rice_sale/jasmine_rice_sale_dao.py
@@ -26,6 +26,23 @@ class CustomersDao(Dao):
         self.get_session.add(customer)
         self.get_session.commit()
 
+    def update_customer(self, id: int, first_name: str = None,
+                        last_name: str = None, gender: str = None, age: int = None):
+        customer = self.get_session.query(Customers).filter(Customers.id == id)
+        if first_name is None:
+            first_name = customer[0].first_name
+        if last_name is None:
+            last_name = customer[0].last_name
+        if gender is None:
+            gender = customer[0].gender
+        if age is None:
+            age = customer[0].age
+        customer.update({Customers.first_name: first_name,
+                         Customers.last_name: last_name,
+                         Customers.gender: gender,
+                         Customers.age: age})
+        self.get_session.commit()
+
 
 class JasmineRiceSaleDao(Dao):
     def get_sale_by_id(self, id: int) -> JasmineRiceSale:
@@ -36,4 +53,20 @@ class JasmineRiceSaleDao(Dao):
 
     def buy_jasmine_rice(self, buy: JasmineRiceSale) -> None:
         self.get_session.add(buy)
+        self.get_session.commit()
+
+    def update_sale(self, id: int, weight_kg: int = None,
+                    time: str = None, customer_id: int = None):
+        sale = self.get_session.query(JasmineRiceSale).filter(JasmineRiceSale.id == id)
+        jasmine_rice_per_kg = 31
+        if weight_kg is None:
+            weight_kg = sale[0].weight_kg
+        if time is None:
+            time = sale[0].time
+        if customer_id is None:
+            customer_id = sale[0].customer_id
+        sale.update({JasmineRiceSale.weight_kg: weight_kg,
+                     JasmineRiceSale.cost: weight_kg * jasmine_rice_per_kg,
+                     JasmineRiceSale.time: time,
+                     JasmineRiceSale.customer_id: customer_id})
         self.get_session.commit()
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from models import JasmineRiceSale
 from jasmine_rice_sale.jasmine_rice_sale_db import JasmineRiceSaleDb
 
+jasmine_rice_per_kg = 31
+jasmine_rice_weight_kg = 50
 
 jasmine_rice_sale = JasmineRiceSaleDb()
 print(jasmine_rice_sale.customer().get_customer_by_id(15))
+print(jasmine_rice_sale.customer().get_all_customers())
+# jasmine_rice_sale.customer().update_customer(id=10, age=18)
 print(jasmine_rice_sale.jasmine_rice_sale().get_sale_by_id(10))
 print(jasmine_rice_sale.jasmine_rice_sale().get_all_sale())
+# jasmine_rice_sale.jasmine_rice_sale().update_sale(id=10, weight_kg=20)
 
-# jasmine_rice_per_kg = 31
-# jasmine_rice_weight_kg = 50
 # buying = JasmineRiceSale(weight_kg=jasmine_rice_weight_kg,
 #                          cost=jasmine_rice_weight_kg * jasmine_rice_per_kg,
 #                          time="114539",
